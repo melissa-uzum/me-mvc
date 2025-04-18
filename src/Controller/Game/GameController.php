15b1febab402e13c7055712ecb4ad7b63df5546d
Melissa Uzum <uzummelisssa@gmail.com>
2025-04-18 12:34:15 +0200

    added game logic files and base twig files

--- src/Controller/Game/GameController.php
+++ src/Controller/Game/GameController.php
@@ -1,5 +1,73 @@
-#[Route("/game", name: "game_index")]
-public function index(): Response
+<?php
+
+namespace App\Controller\Game;
+
+use App\Game\Game21;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class GameController extends AbstractController
 {
-    return $this->render('game/index.html.twig');
+    #[Route('/game', name: 'game_start')]
+    public function start(): Response
+    {
+        return $this->render('game/index.html.twig');
+    }
+
+    #[Route('/game/doc', name: 'game_doc')]
+    public function doc(): Response
+    {
+        return $this->render('game/doc.html.twig');
+    }
+
+    #[Route('/game/play', name: 'game_play')]
+    public function play(SessionInterface $session): Response
+    {
+        $game = $session->get('game21') ?? new Game21();
+        $session->set('game21', $game);
+
+        return $this->render('game/play.html.twig', [
+            'playerHand' => $game->getPlayerHand()->getCards(),
+            'bankHand' => $game->isPlayerStanding() || $game->isGameOver()
+                ? $game->getBankHand()->getCards() : [],
+            'playerValue' => $game->getPlayerValue(),
+            'bankValue' => $game->isPlayerStanding() || $game->isGameOver()
+                ? $game->getBankValue() : null,
+            'gameOver' => $game->isGameOver(),
+            'winner' => $game->getWinner(),
+        ]);
+    }
+
+    #[Route('/game/draw', name: 'game_draw', methods: ['POST'])]
+    public function draw(SessionInterface $session): Response
+    {
+        /** @var Game21 $game */
+        $game = $session->get('game21');
+        $game->playerDraw();
+
+        $session->set('game21', $game);
+        return $this->redirectToRoute('game_play');
+    }
+
+    #[Route('/game/stand', name: 'game_stand', methods: ['POST'])]
+    public function stand(SessionInterface $session): Response
+    {
+        /** @var Game21 $game */
+        $game = $session->get('game21');
+        $game->playerStands();
+        $game->bankTurn();
+
+        $session->set('game21', $game);
+        return $this->redirectToRoute('game_play');
+    }
+
+    #[Route('/game/reset', name: 'game_reset', methods: ['POST'])]
+    public function reset(SessionInterface $session): Response
+    {
+        $session->remove('game21');
+        return $this->redirectToRoute('game_play');
+    }
 }
